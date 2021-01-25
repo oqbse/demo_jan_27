b2675efe338e87afb90d78ceea25023693ba8f30
Oscar Bian <oscar.bian@medengine.co>
2021-01-25 22:16:07 +0100

    oops

---
+++ src/screens/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home: React.FC = () => {
+
+    const [url, setUrl] = React.useState<string>('')
+
+    React.useEffect(() => {
+        const fetchImageOfTheDay = async () => {
+            try {
+             const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=WP2E9jSNX6gwUM4Avw8XtPyuneDW9Fv6gTv0TfiC')
+             const parsedResponse = await response.json()
+             setUrl(parsedResponse.hdurl)
+            } catch (error) {
+                console.error(error)
+            }
+    }
+    fetchImageOfTheDay()
+}, []);
+
+return (
+    <>
+    <h1>Welcome</h1>
+    <img src={url} alt="Image" />
+    </>
+)}
+
+export default Home;
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Profile: React.FC = () => (
+    <h1>My profile</h1>
+)
+
+export default Profile;
